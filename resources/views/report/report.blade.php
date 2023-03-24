51bed9809ea5739350a0eb89caca4ca3fb7392da
khaledfathi <khaledfathi@protonmail.com>
2023-03-24 17:49:24 +0200

    checkpoint

--- resources/views/report/report.blade.php
+++ resources/views/report/report.blade.php
@@ -10,17 +10,78 @@
 
 
 @section('content')
-    <h1>REPORT PAGE</h1>
-    <p>transaction for this day with total</p>
-    <p>transaction for this month with total</p>
-    <p>records in all tables for this day</p>
-    <p>count products in count category</p>
-    <p>customer count</p>
-    <p>project count status</p>
-    <p>open project count</p>
-    <p>project near to end (3 days)</p>
+<div class="container">
+    <div style="border:1px solid black;">
+        <p>الحركات المالية اليوم</p>        
+        <table>
+            <thead>
+                <th>نوع العملية</th>
+                <th>رقم العملية</th>
+                <th>تاريخ</th>
+                <th>وقت</th>
+                <th>رقم العميل</th>
+                <th>رقم المشروع</th>
+                <th>الحركة</th>
+            </thead>
+        </table>
+    </div>
 
+    <div style="border:1px solid black;">
+        <h3>رصيد اليوم (100 جنية )</h3>
+        <p>ايرادات | مصروفات</p>
+    </div>
 
+    <div style="border:1px solid black;">
+        <h3>رصيد الشهر الحالى (مارس 2023) - (1000 جنية)</h3>
+        <p>ايرادات | مصروفات</p>
+    </div>
 
+    <div style="border:1px solid black;">
+        <h3>عمليات اليوم</h3>
+    </div>
+
+    <div style="border:1px solid black;">
+        <h3>عدد العملاء المسجلين (320) عميل</h3>
+    </div>
+
+    <div style="border:1px solid black;">
+        <h3>عدد المنتجات المسجلة</h3>
+    </div>
+
+    <div style="border:1px solid black;">
+        <h3>عدد الاصناف المسجلة</h3>
+    </div>
+
+    <div>
+        <h3>اجمالى اعداد المنتجات المتاحة</h3>
+    </div>
+
+    <div style="border:1px solid black;">
+        <h3>عدد المشاريع المسجلة (902) مشروع</h3>
+    </div>
+
+    <div style="border:1px solid black;">
+        <h3> المشاريع المفتوحة</h3>
+    </div>
+
+    <div style="border:1px solid black;">
+        <h3>مشاريع اقترب ميعاد تسليمها (5 ايام)</h3>
+    </div>
+
+    <div style="border:1px solid black;">
+        <h3>مشاريع منتهية لم يتم تسليمها</h3>
+    </div>
+
+    <div style="border:1px solid black;">
+        <h3>مشاريع لديها مديونية</h3>
+    </div>
+
+    <div style="border:1px solid black;">
+        <h3>مشاريع مؤجلة</h3>
+    </div>
+
+</div>
 @endsection
-    
+    {{-- 
+    <p>count products in count category</p>
+    <p>project near to end (3 days)</p> --}}
--- resources/views/setting/setting.blade.php
+++ resources/views/setting/setting.blade.php
@@ -12,11 +12,8 @@
 @section('content')
     <div class="container">
         <a href="{{url('usersmanagment')}}">ادارة المستخدمين</a>
+        <p>change logo</p>
+        <p>clear/empty database</p>
     </div>
-    <h1>SETTING PAGE</h1>
-    <p>app info [ver .. etc]</p>
-    <p>change logo</p>
-    <p>process monitoring</p>
-    <p>Manage All Tables</p>
 @endsection
     
